8527ba495ae0191ea3b4cb97635b18f7ee6f4dbe
bruvini <86611700+bruvini@users.noreply.github.com>
2022-12-20 23:03:38 -0300

    Atualizando itens

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const form = document.getElementById('novoItem')
 const lista = document.getElementById('lista')
 const itens = JSON.parse(localStorage.getItem('itens')) || []
 
-itens.forEach( (elemento) => {
+itens.forEach((elemento) => {
     criaElemento(elemento)
 })
 
@@ -13,16 +13,26 @@ form.addEventListener('submit', (evento) => {
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
 
+    const existe = itens.find(elemento => elemento.Nome === nome.value)
+    console.log(existe)
+
     //armazenar no local storage
     const itemAtual = {
         'Nome': nome.value,
         'Quantidade': quantidade.value
     }
 
-    //obter os valores dos inputs e insere na função de criar elemento
-    criaElemento(itemAtual)
-    
-    itens.push(itemAtual)
+    if (existe) {
+        itemAtual.id = existe.id
+        atualizaElemento(itemAtual)
+        itens[existe.id] = itemAtual
+    } else {
+        itemAtual.id = itens.length
+        //obter os valores dos inputs e insere na função de criar elemento
+        criaElemento(itemAtual)
+        itens.push(itemAtual)
+    }
+
     localStorage.setItem('itens', JSON.stringify(itens))
 
     //apaga o input depois de enviado os dados
@@ -37,8 +47,13 @@ function criaElemento(item) {
 
     const numeroItem = document.createElement('strong')
     numeroItem.innerHTML = item.Quantidade
+    numeroItem.dataset.id = item.id
     novoItem.append(numeroItem)
 
     novoItem.innerHTML += item.Nome
-    lista.appendChild(novoItem)    
+    lista.appendChild(novoItem)
+}
+
+function atualizaElemento(item){
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.Quantidade
 }
