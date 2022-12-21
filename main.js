86c8f2e24df109861d10de1044a6a21d845e4286
bruvini <86611700+bruvini@users.noreply.github.com>
2022-12-20 22:00:30 -0300

    Atualizando página sem perder itens com Local Storage

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const form = document.getElementById('novoItem')
 const lista = document.getElementById('lista')
-const itens = []
+const itens = JSON.parse(localStorage.getItem('itens')) || []
+
+itens.forEach( (elemento) => {
+    criaElemento(elemento)
+})
 
 //cria ação para obter os dados enviados ao apertar o submit
 form.addEventListener('submit', (evento) => {
@@ -9,8 +13,17 @@ form.addEventListener('submit', (evento) => {
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
 
+    //armazenar no local storage
+    const itemAtual = {
+        'Nome': nome.value,
+        'Quantidade': quantidade.value
+    }
+
     //obter os valores dos inputs e insere na função de criar elemento
-    criaElemento(nome.value, quantidade.value)
+    criaElemento(itemAtual)
+    
+    itens.push(itemAtual)
+    localStorage.setItem('itens', JSON.stringify(itens))
 
     //apaga o input depois de enviado os dados
     nome.value = ''
@@ -18,22 +31,14 @@ form.addEventListener('submit', (evento) => {
 })
 
 //função para criar item
-function criaElemento(nome, qtd) {
+function criaElemento(item) {
     const novoItem = document.createElement('li')
     novoItem.classList.add('item')
-    const numeroItem = document.createElement('strong')
-    numeroItem.innerHTML = qtd
-    novoItem.append(numeroItem)
-    novoItem.innerHTML += nome
-    lista.appendChild(novoItem)
 
-    //armazenar no local storage
-    const itemAtual = {
-        'Nome': nome,
-        'Quantidade': qtd
-    }
-    
-    itens.push(itemAtual)
-    localStorage.setItem('Item', JSON.stringify(itens))
-    
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = item.Quantidade
+    novoItem.append(numeroItem)
+
+    novoItem.innerHTML += item.Nome
+    lista.appendChild(novoItem)    
 }
